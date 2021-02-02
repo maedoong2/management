ef250e9f5e22a05ee1d5836bdf152c9e0ddedfd2
programong <dasol9987@naver.com>
2021-02-02 11:25:03 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Customer from './components/Customer.js';
+import Customer from './components/Customer';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -20,36 +20,31 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '김다솔',
-    'birthday' : '930310',
-    'gender' : '여자',
-    'job' : '개발자'
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : '장발장',
-    'birthday' : '900124',
-    'gender' : '남자',
-    'job' : '디자이너'
-  }
-]
-
 class App extends Component {
+
+  //state는 component에 대해서 변경될 수 있는 변수를 처리할 때 쓴다.
+  state = {
+    customers: ""
+  }
+
+  //api에 접근해서 데이터를 받아오는 작업을 한다.
+  //component가 mount를 완료 했을 때, 실행되는 부분. 
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers:res})) //callApi에서 받은 데이터(res)를 state의 customers에 넣어준다.
+      .catch(err => console.log(err));
+  }
+//api를 불러온다. 비동기적으로 수행한다.
+  callApi = async () => {
+    //해당 주소로 접근해서.
+    const response = await fetch('/api/customers');
+    //json 형태로 받아서 body라는 변수로 받아오고 그것을 리턴한다.
+    const body = await response.json();
+    return body;
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes } = this.props; //props는 변경될 수 없는 데이터를 명시할 때 쓴다.
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -62,7 +57,9 @@ class App extends Component {
             <TableCell>직업</TableCell>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/> ); }) }
+            {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/> ); 
+            }) : ""}  
+            {/* this.state.customers가 있으면 돌고, 없으면 공백으로 처리하겠다. */}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -3,12 +3,38 @@ const bodyParser = require('body-parser'); //서버 모듈을 위한 기능
 const app = express();
 const port = process.env.PORT || 5000; //서버의 포트번호
 
+//bodyParser json 데이터 형식 설정
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-}); //클라이언트 즉 이 서버에 접속하는 사용자 입장에서 이 /api/hello로 접속했을 때, 어떤 처리를 해줄 것인가를 정함.
-
+//restapi 데이터 설정
+app.get('/api/customers', (req, res) =>{
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '홍길동',
+        'birthday' : '961222',
+        'gender' : '남자',
+        'job' : '대학생'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '김다솔',
+          'birthday' : '930310',
+          'gender' : '여자',
+          'job' : '개발자'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '장발장',
+          'birthday' : '900124',
+          'gender' : '남자',
+          'job' : '디자이너'
+        }
+      ]);
+});
 app.listen(port, () => console.log(`Listening on port ${port}`)); //서버가 동작중이라면 메세지를 뜨게 한다.
 //cf) 숫자 1 옆의 ` 특수문자를 사용해줘야 변수를 넣을 수 있다.
